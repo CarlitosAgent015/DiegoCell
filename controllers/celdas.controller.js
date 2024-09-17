8d6bd0120d76bb5790efd6a91b596728331aa70c
Carlos <doctorstroum@gmail.com>
2024-09-17 09:18:18 -0500

    comit algo

---
+++ controllers/celdas.controller.js
@@ -0,0 +1,129 @@
+import Celda from '../models/celdas.js';
+
+export async function getCelda(req, res) {
+    try {
+        const celdas = await Celda.find().select('numberCelda estado');
+        res.json(celdas);
+    } catch (error) {
+        res.status(500).json({ message: 'Error getting cells', error });
+    }
+};
+
+export async function getCeldasByEstado(req, res) {
+    try {
+        const celdas = await Celda.find({ estado: req.params.estado});
+        res.json(celdas);
+    }
+    catch (error){
+        res.status(500).json({msg: 'Error getting cells by state', error});
+    }
+};
+
+export async function getCeldasById(req, res) {
+    try{
+        const celda = await Celda.findById(req.params.id);
+        if(!celda) return res.status(400).json({msg: 'Cell not found'});
+        res.json(celda);
+    }
+    catch(error){
+        res.status(500).json({msg: 'Error getting cell', error});
+    }  
+};
+
+export async function postCelda(req, res) {
+    try {
+        const count = await Celda.countDocuments();
+        if (count >= 10) {
+            return res.status(400).json({ message: 'Cell limit reached.' });
+        }
+        const lastCelda = await Celda.findOne().sort({ numberCelda: -1 });
+        const numeroCelda = lastCelda ? lastCelda.numberCelda + 1 : 1;
+
+        const newCelda = new Celda({
+            numberCelda: numeroCelda,
+            estado: 'available'
+        });
+
+        await newCelda.save();
+        res.json({ message: 'Created cell'});
+    } 
+    catch (error) {
+        res.status(500).json({ message: 'Error creating cell', error });
+    }
+}
+
+export async function putCelda(req, res) {
+    try {
+        const putCelda = await Celda.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!putCelda) return res.status(404).json({ message: 'Cell not found' });
+
+        res.json({ message: 'Cell updated successfully' });
+    } catch (error) {
+        res.status(500).json({ message: 'Error updating cell', error });
+    }
+}
+
+export async function deleteCelda(req, res) {
+    let msg = 'Deleted cell';
+    try {
+        await Celda.findByIdAndDelete(req.params.id);
+    } catch (error) {
+        msg = 'There was a problem deleting the cell';
+    }
+    res.json({ msg: msg });
+};
+
+export async function parkVehicle(req, res) {
+    try {
+        const celda = await Celda.findOne({ estado: 'available' }).sort({ numberCelda: 1 });
+        if (!celda) return res.status(404).json({ message: 'No cells available' });
+
+        const { plateVehicle } = req.body;
+        if (!plateVehicle) return res.status(400).json({ message: 'Vehicle license plate is required' });
+
+        celda.estado = 'not available';
+        celda.plateVehicle = plateVehicle;
+        celda.entry_date = new Date();
+        celda.pin = null;
+
+        await celda.save();
+        res.json({ msg: 'Parked vehicle', celda });
+    }
+    catch (error) {
+        res.status(500).json({ message: 'Error when parking vehicle', error: error.message });
+    }
+};
+
+
+export async function goOutVehicle(req, res) {
+    try {
+        const celda = await Celda.findById(req.params.id);
+        if (!celda) return res.status(404).json({ message: 'Cell not found' });
+        if (celda.estado !== 'not available') return res.status(400).json({ message: 'The cell is not occupied' });
+
+        const now = new Date();
+        const horas = Math.max(1, Math.floor((now - celda.entry_date) / (1000 * 60 * 60)));
+        const valor_a_pagar = horas * 5000;
+
+        // Restablecer todos los campos relacionados con el vehículo estacionado
+        celda.estado = 'available';  // La celda vuelve a estar disponible
+        celda.plateVehicle = null;   // Limpiar la placa del vehículo
+        celda.entry_date = null;     // Limpiar la fecha de entrada
+        celda.departure_date = null; // Limpiar la fecha de salida
+        celda.pin = null;            // Limpiar cualquier PIN asociado
+
+
+        await celda.save();
+
+        // Responder con la información del valor a pagar y las fechas
+        res.json({
+            message: 'The vehicle has left, and the cell is reset',
+            valor_a_pagar,
+            fecha_entrada: null,  // Restablecido a null
+            fecha_salida: null    // Restablecido a null
+        });
+    } catch (error) {
+        res.status(500).json({ message: 'Error when registering vehicle exit', error });
+    }
+}
+
---
+++ routes/celdaRouter.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import {getCelda, getCeldasByEstado, getCeldasById, postCelda, putCelda, deleteCelda, parkVehicle, goOutVehicle} from '../controllers/celdas.controller.js';
+
+const celdaRouter = Router();
+
+
+celdaRouter.get('/cell', getCelda); 
+celdaRouter.get('/:id', getCeldasById); 
+celdaRouter.get('/estado/:estado', getCeldasByEstado); 
+celdaRouter.post('/', postCelda);
+celdaRouter.put('/:id', putCelda); 
+celdaRouter.delete('/:id', deleteCelda); 
+celdaRouter.post('/parkVehicle', parkVehicle);
+celdaRouter.post('/go_out/:id', goOutVehicle);
+
+export default celdaRouter;
